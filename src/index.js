6d59bf414deae0260c141a03c72bbe79df01811b
julian <julian7454@gmail.com>
2019-03-31 15:46:26 +0800

    feat: click button set value state

--- src/index.js
+++ src/index.js
@@ -4,8 +4,19 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 class Square extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: null
+    };
+  }
+
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button className="square" onClick={() => this.setState({ value: "X" })}>
+        {this.state.value}
+      </button>
+    );
   }
 }
 
